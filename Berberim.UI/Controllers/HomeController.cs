9e03eac9540f8180d2a98eb6d230728d246b2b6f
Atakan Gemici <atakan.gemici@ofisim.com>
2018-07-03 00:48:23 +0300

    yeni tasarım

--- Berberim.UI/Controllers/HomeController.cs
+++ Berberim.UI/Controllers/HomeController.cs
@@ -23,7 +23,8 @@ namespace Berberim.UI.Controllers
             {
                 berber = _db.BerberSayfa.Where(i => i.IsActive == true).ToList(),
                 kampanyalar = _db.Kampanyalar.Where(i => i.IsActive == true).ToList(),
-                trendSac = _db.TrendSaclar.Where(i => i.IsActive == true).ToList()
+                trendSac = _db.TrendSaclar.Where(i => i.IsActive == true).ToList(),
+                //musteriYorumlar = _db.MusteriYorumlari.Where(i => i.IsActive == true).ToList().Take(5).ToList()
             };
             return View(data);
         }
