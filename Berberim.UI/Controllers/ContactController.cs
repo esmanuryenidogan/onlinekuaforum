84446d23d8d58527b8e7eadf04b933e74b313ba2
Atakan Gemici <atakan.gemici@ofisim.com>
2018-10-07 02:31:43 +0300

    email smtp ayarları ponçiğin bu saatte çalışıyoo

--- Berberim.UI/Controllers/ContactController.cs
+++ Berberim.UI/Controllers/ContactController.cs
@@ -3,6 +3,9 @@ using Berberim.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -24,6 +27,33 @@ namespace Berberim.UI.Controllers
                 musteriYorum = _db.YORUM.Where(i => i.STATUS == Constants.RecordStatu.Active).ToList().Take(5).ToList()
             };
             return View(data);
+        }
+
+        public async Task<ActionResult> ContactEmail()
+        {
+            var body = "";
+            var message = new MailMessage();
+            message.To.Add(new MailAddress("atakan.gemici@ofisim.com"));  // replace with valid value 
+            message.From = new MailAddress("atakangmc@gmail.com");  // replace with valid value
+            message.Subject = "Your email subject";
+            message.Body = "<h1>atakan<h1>";
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credential = new NetworkCredential
+                {
+                    UserName = "38d96fd3370aa2",  // replace with valid value
+                    Password = "591628a8bef258"  // replace with valid value
+                }; 
+
+                smtp.Credentials = credential;
+                smtp.Host = "smtp.mailtrap.io";
+                smtp.Port = 465;
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+                return View("Index");
+            }
         }
     }
 }
